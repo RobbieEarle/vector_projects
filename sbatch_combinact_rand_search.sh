1e56c8efd67aeab9c5e985a096f5308e3c839bfc
rearle <rb504035@dal.ca>
2020-02-28 20:25:52 -0400

    Changed number of jobs

--- sbatch_combinact_rand_search.sh
+++ sbatch_combinact_rand_search.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 4                   # number of CPU cores
 #SBATCH --mem=8G               # memory per node
 #SBATCH --time=12:00:00        # max walltime, hh:mm:ss
-#SBATCH --array=0-999%10       # array value
+#SBATCH --array=0-500%10       # array value
 #SBATCH --output=logs/combinact/%a-%N-%j    # %N for node name, %j for jobID
 #SBATCH --job-name=combinact_rw
 
