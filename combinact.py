d08acfab0289f90488fd5fd1b15fb1a96dfa6b8e
rearle <rb504035@dal.ca>
2020-03-09 00:27:26 -0300

    combinact.py is now ready to run on vector. sbatch file is finished as well.

--- combinact.py
+++ combinact.py
@@ -51,13 +51,132 @@ _ACTFUNS2D = {
 }
 
 
+def get_n_params(model):
+    pp=0
+    for p in list(model.parameters()):
+        nn=1
+        for s in list(p.size()):
+            nn = nn*s
+        pp += nn
+    return pp
+
+
+def seed_all(seed=None, only_current_gpu=False, mirror_gpus=False):
+    r"""
+    Initialises the random number generators for random, numpy, and both CPU and GPU(s)
+    for torch.
+    Arguments:
+        seed (int, optional): seed value to use for the random number generators.
+            If :attr:`seed` is ``None`` (default), seeds are picked at random using
+            the methods built in to each RNG.
+        only_current_gpu (bool, optional): indicates whether to only re-seed the current
+            cuda device, or to seed all of them. Default is ``False``.
+        mirror_gpus (bool, optional): indicates whether all cuda devices should receive
+            the same seed, or different seeds. If :attr:`mirror_gpus` is ``False`` and
+            :attr:`seed` is not ``None``, each device receives a different but
+            deterministically determined seed. Default is ``False``.
+    Note that we override the settings for the cudnn backend whenever this function is
+    called. If :attr:`seed` is not ``None``, we set::
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+    in order to ensure experimental results behave deterministically and are repeatible.
+    However, enabling deterministic mode may result in an impact on performance. See
+    `link`_ for more details. If :attr:`seed` is ``None``, we return the cudnn backend
+    to its performance-optimised default settings of::
+        torch.backends.cudnn.deterministic = False
+        torch.backends.cudnn.benchmark = True
+    .. _link:
+        https://pytorch.org/docs/stable/notes/randomness.html
+    """
+    # Note that random, np.random and torch's RNG all have different
+    # implementations so they will produce different numbers even with
+    # when they are seeded the same.
+
+    # Seed Python's built-in random number generator
+    random.seed(seed)
+    # Seed numpy's random number generator
+    np.random.seed(seed)
+
+    def get_seed():
+        '''
+        On Python 3.2 and above, and when system sources of randomness are
+        available, use `os.urandom` to make a new seed. Otherwise, use the
+        current time.
+        '''
+        try:
+            import os
+            # Use system's source of entropy (on Linux, syscall `getrandom()`)
+            s = int.from_bytes(os.urandom(4), byteorder="little")
+        except AttributeError:
+            from datetime import datetime
+            # Get the current time in mircoseconds, and map to an integer
+            # in the range [0, 2**32)
+            s = int((datetime.utcnow() - datetime(1970, 1, 1)).total_seconds()
+                    * 1000000) % 4294967296
+        return s
+
+    # Seed pytorch's random number generator on the CPU
+    # torch doesn't support a None argument, so we have to source our own seed
+    # with high entropy if none is given.
+    s = seed if seed is not None else get_seed()
+    torch.manual_seed(s)
+
+    if seed is None:
+        # Since seeds are random, we don't care about determinism and
+        # will set the backend up for optimal performance
+        torch.backends.cudnn.deterministic = False
+        torch.backends.cudnn.benchmark = True
+    else:
+        # Ensure cudNN is deterministic, so the results are consistent
+        # for this seed
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    # Seed pytorch's random number generator on the GPU(s)
+    if only_current_gpu:
+        # Only re-seed the current GPU
+        if mirror_gpus:
+            # ... re-seed with the same as the CPU seed
+            torch.cuda.manual_seed(s)
+        elif seed is None:
+            # ... re-seed at random, however pytorch deems fit
+            torch.cuda.seed()
+        else:
+            # ... re-seed with a deterministic seed based on, but
+            # not equal to, the CPU seed
+            torch.cuda.manual_seed((seed + 1) % 4294967296)
+    elif mirror_gpus:
+        # Seed multiple GPUs, each with the same seed
+        torch.cuda.manual_seed_all(s)
+    elif seed is None:
+        # Seed multiple GPUs, all with unique seeds
+        # ... a random seed for each GPU, however pytorch deems fit
+        torch.cuda.seed_all()
+    else:
+        # Seed multiple GPUs, all with unique seeds
+        # ... different deterministic seeds for each GPU
+        # We assign the seeds in ascending order, and can't exceed the
+        # random state's maximum value of 2**32 == 4294967296
+        for device in range(torch.cuda.device_count()):
+            with torch.cuda.device(device):
+                torch.cuda.manual_seed((seed + 1 + device) % 4294967296)
+
+
 def test_net_inputs(actfun, in_size, M1, M2, out_size, k1, k2, p1, p2, g2, g_out):
-    if M1 * p1 % k1 != 0:
-        return 'k1 must divide M1*p1.  k1 = {}, M1*p1 = {}'.format(k1, M1 * p1)
     if M2 % g2 != 0:
         return 'g2 must divide M2.  M2 = {}, g2 = {}'.format(M2, g2)
+    if out_size % g_out != 0:
+        return 'g_out must divide out_size.  out_size = {}, g_out = {}'.format(out_size, g_out)
+    if (M1 * p1) % k1 != 0:
+        return 'k1 must divide M1*p1.  k1 = {}, M1*p1 = {}'.format(k1, M1 * p1)
+    if (M2 * p2) % k2 != 0:
+        return 'k2 must divide M2*p2.  k2 = {}, M2*p2 = {}'.format(k2, M2 * p2)
+    if (M2 / g2) % k2 != 0:
+        return 'k2 must divide M2/g2.  k2 = {}, M2/g2 = {}'.format(k2, M2 / g2)
     if (M1 * p1 / k1) % g2 != 0:
         return 'g2 must divide M1*p1/k1.  M1*p1/k1 = {}, g2 = {}'.format(M1 * p1 / k1, g2)
+    if (M2 * p2 / k2) % g_out != 0:
+        return 'g_out must divide M2*p2/k2.  M2*p2/k2 = {}, g_out = {}'.format(M2 * p2 / k2, g_out)
     return None
 
 
@@ -140,12 +259,12 @@ class ScottNet(nn.Module):
             x = self.activate(self.bn1(self.fc1(x)), self.M1, self.k1, self.p1)
 
             # Grouping: [batch_size, M1 / k1, p1] -> [batch_size, (M1 * p1 / k1) / g2, g2]
-            x = torch.transpose(x, dim0=1, dim1=2).reshape(
-                (self.batch_size, int((self.M1*self.p1/self.k1) / self.g2), self.g2))
+            x = torch.transpose(x, dim0=1, dim1=2).reshape(self.batch_size,
+                                                           int((self.M1*self.p1/self.k1) / self.g2), self.g2)
 
             # ------- Second Round
             #   In: [batch_size, (M1 * p1 / k1) / g2, g2]
-            #   Out: [batch_size, (M2 / g2) / k2, p2 * g2]
+            #   Out: [batch_size, (M2 / k2) / g2, p2 * g2]
             outputs = torch.zeros((self.batch_size, int((self.M2 / self.g2) / self.k2), self.p2 * self.g2),
                                   device=x.device)
             for i, fc2 in enumerate(self.r2_fc_groups):
@@ -154,8 +273,8 @@ class ScottNet(nn.Module):
             x = outputs
 
             # Grouping: [batch_size, (M2 / g2) / k2, p2 * g2] -> [batch_size, (M2 * p2 / k2) / g_out, g_out]
-            x = torch.transpose(x, dim0=1, dim1=2).reshape(
-                (self.batch_size, int((self.M2 * self.p2 / self.k2) / self.g_out), self.g_out))
+            x = torch.transpose(x, dim0=1, dim1=2).reshape(self.batch_size,
+                                                           int((self.M2 * self.p2 / self.k2) / self.g_out), self.g_out)
 
             # ------- Third Round
             #   In: [batch_size, (M2 * p2 / k2) / g_out, g_out]
@@ -207,7 +326,7 @@ def weights_init(m):
         m.bias.data.fill_(0)
 
 
-def train_model(model, train_loader, validation_loader, hyper_params):
+def train_model(model, outfile_path, fieldnames, seed, train_loader, validation_loader, hyper_params):
 
     # ---- Initialization
     model.apply(weights_init)
@@ -228,8 +347,9 @@ def train_model(model, train_loader, validation_loader, hyper_params):
 
     # ---- Start Training
     epoch = 1
-    while epoch <= 10:
+    while epoch <= 2:
 
+        start_time = time.time()
         final_train_loss = 0
         # ---- Training
         for batch_idx, (x, targetx) in enumerate(train_loader):
@@ -267,10 +387,31 @@ def train_model(model, train_loader, validation_loader, hyper_params):
                 .format(model.actfun, epoch, final_train_loss, final_val_loss, accuracy), flush=True
         )
 
+        # Outputting data to CSV at end of epoch
+        with open(outfile_path, mode='a') as out_file:
+            writer = csv.DictWriter(out_file, fieldnames=fieldnames, lineterminator='\n')
+            writer.writerow({'seed': seed,
+                             'epoch': epoch,
+                             'actfun': model.actfun,
+                             'train_loss': float(final_train_loss),
+                             'val_loss': float(final_val_loss),
+                             'acc': float(accuracy),
+                             'time': (time.time() - start_time),
+                             'M1': model.M1,
+                             'M2': model.M2,
+                             'k1': model.k1,
+                             'k2': model.k2,
+                             'p1': model.p1,
+                             'p2': model.p2,
+                             'g2': model.g2,
+                             'g_out': model.g_out,
+                             'n_params': get_n_params(model)
+                             })
+
         epoch += 1
 
 
-def run_experiment():
+def run_experiment(actfun, seed, outfile_path):
 
     trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
     mnist_train_full = datasets.MNIST(root='./data', train=True, download=True, transform=trans)
@@ -283,12 +424,39 @@ def run_experiment():
     train_loader = torch.utils.data.DataLoader(dataset=mnist_train, batch_size=batch_size, shuffle=True, pin_memory=True)
     validation_loader = torch.utils.data.DataLoader(dataset=mnist_validation, batch_size=batch_size, shuffle=True, pin_memory=True)
 
-    model = ScottNet(batch_size=batch_size)
+    rng = np.random.RandomState(seed)
+    while True:
+        M1 = rng.randint(60, 120) * 2
+        M2 = rng.randint(60, 120) * 2
+        k1 = rng.randint(2, 11)
+        k2 = rng.randint(2, 11)
+        p1 = rng.randint(1, 11)
+        p2 = rng.randint(1, 11)
+        g2 = rng.randint(1, 5)
+        g_out = rng.randint(1, 5)
+        if test_net_inputs(actfun, 784, M1, M2, 10, k1, k2, p1, p2, g2, g_out) is None:
+            break
+    model = ScottNet(batch_size=100, actfun=actfun, in_size=784, out_size=10,
+                     M1=M1, M2=M2, k1=k1, k2=k2, p1=p1,
+                     p2=p2, g2=g2, g_out=g_out)
+    if torch.cuda.is_available():
+        model = model.cuda()
+
+    print(
+        "Model Params: M1 = {] | M2 = {} | k1 = {} | k2 = {} | p1 = {} | p2 = {} | g2 = {} | g_out = {} | params = {}"
+            .format(M1, M2, k1, k2, p1, p2, g2, g_out, get_n_params(model)), flush=True
+    )
+
+    fieldnames = ['seed', 'epoch', 'actfun', 'train_loss', 'val_loss', 'acc', 'time',
+                  'M1', 'M2', 'k1', 'k2', 'p1', 'p2', 'g2', 'g_out', 'n_params']
+    with open(outfile_path, mode='w') as out_file:
+        writer = csv.DictWriter(out_file, fieldnames=fieldnames, lineterminator='\n')
+        writer.writeheader()
 
     hyper_params = {"l2": {"adam_beta_1": 0.760516,
                            "adam_beta_2": 0.999983,
-                           "adam_eps": 1.7936 * 10 **-8,
-                           "adam_wd": 1.33755 * 10 **-5,
+                           "adam_eps": 1.7936 * 10 ** -8,
+                           "adam_wd": 1.33755 * 10 ** -5,
                            "max_lr": 0.0122491,
                            "cycle_peak": 0.234177
                            },
@@ -337,10 +505,25 @@ def run_experiment():
                     }
 
     print("Running...")
-    train_model(model, train_loader, validation_loader, hyper_params['l2'])
+    train_model(model, outfile_path, fieldnames, seed, train_loader, validation_loader, hyper_params[actfun])
     print()
 
 
 if __name__ == '__main__':
 
-    run_experiment()
+    if len(sys.argv) == 1:
+        actfun = "max"
+        seed = 0
+        outfile_path = str(datetime.date.today()) + "-combinact-" + str(actfun) + "-" + str(seed) + ".csv"
+
+    else:
+        seed_all(0)
+        actfun = sys.argv[1]
+        seed = int(sys.argv[2])
+        outfile_path = sys.argv[3] + "/" + str(datetime.date.today()) + "-combinact-"\
+                       + str(actfun) + "-" + str(seed) + ".csv"
+
+    print("Activation Function: " + str(actfun))
+    print("Save Path: " + str(outfile_path))
+
+    run_experiment(actfun, seed, outfile_path)
---
+++ sbatch_combinact.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -p p100,max12hours     # partition - should be gpu on MaRS (q), and either p100 or t4 on Vaughan (vremote1)
+#SBATCH --gres=gpu:1           # request GPU(s)
+#SBATCH -c 4                   # number of CPU cores
+#SBATCH --mem=8G               # memory per node
+#SBATCH --time=12:00:00        # max walltime, hh:mm:ss
+#SBATCH --array=0-500%10       # array value
+#SBATCH --output=logs/combinact/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=combinact
+
+source ~/.bashrc
+source activate ~/venvs/combinact
+
+SAVE_PATH="$1"
+SEED="$SLURM_ARRAY_TASK_ID"
+
+# Debugging outputs
+pwd
+which conda
+python --version
+pip freeze
+
+echo ""
+python -c "import torch; print('torch version = {}'.format(torch.__version__))"
+python -c "import torch.cuda; print('cuda = {}'.format(torch.cuda.is_available()))"
+echo ""
+
+echo "SAVE_PATH=$SAVE_PATH"
+echo "SEED=$SEED"
+
+~/utilities/log_gpu_cpu_stats -l 0.5 -n 500 -t "logs/combinact/${SLURM_ARRAY_TASK_ID}_${SLURM_NODEID}_${SLURM_ARRAY_JOB_ID}_compute_usage.log"&
+export LOGGER_PID="$!"
+
+python combinact.py "relu" "$SEED" "$SAVE_PATH" &
+python combinact.py "max" "$SEED" "$SAVE_PATH" &
+python combinact.py "signed_geomean" "$SEED" "$SAVE_PATH" &
+python combinact.py "swish2" "$SEED" "$SAVE_PATH" &
+python combinact.py "l2" "$SEED" "$SAVE_PATH" &
+python combinact.py "linf" "$SEED" "$SAVE_PATH" &
+python combinact.py "zclse-approx" "$SEED" "$SAVE_PATH"
+
+kill "$LOGGER_PID"
