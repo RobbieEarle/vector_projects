97e05c3b1f8723b720310678310b1eb3ecf7efc3
rearle <rb504035@dal.ca>
2020-08-09 16:11:10 -0300

    Job: e2_pk_exp
    Running pk experiments using newly optimized actfuns

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=8G               # memory per node
 #SBATCH --time=20:00:00        # max walltime, hh:mm:ss
 #SBATCH --array=0-49%10        # array value
-#SBATCH --output=logs/e2_param_eff/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e2_param_eff
+#SBATCH --output=logs/e2_pk_exp/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e2_pk_exp
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -33,7 +33,7 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --model nn --dataset mnist --actfun all_pk --p_param_eff --var_n_params
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset mnist --actfun all_pk --p_param_eff --var_n_params
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar10 --actfun all_pk --p_param_eff --var_n_params
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar100 --actfun all_pk --p_param_eff --var_n_params
+python engine.py --seed $SEED --save_path $SAVE_PATH --actfun all_pk_new --dataset cifar10 --model cnn --var_k
+python engine.py --seed $SEED --save_path $SAVE_PATH --actfun all_pk_new --dataset cifar10 --model cnn --var_p
+python engine.py --seed $SEED --save_path $SAVE_PATH --actfun all_pk_new --dataset cifar100 --model cnn --var_k
+python engine.py --seed $SEED --save_path $SAVE_PATH --actfun all_pk_new --dataset cifar100 --model cnn --var_p
