924c873291916011f73f9e1ae69b920336792d4d
rearle <rb504035@dal.ca>
2020-08-12 02:27:29 -0300

    Job: e3_rands_bin3
    Third random search for binary ops

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=8G               # memory per node
 #SBATCH --time=20:00:00        # max walltime, hh:mm:ss
 #SBATCH --array=0-95%12        # array value
-#SBATCH --output=logs/e3_rands_bin2/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e3_rands_bin2
+#SBATCH --output=logs/e3_rands_bin3/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e3_rands_bin3
 
 source ~/.bashrc
 source activate ~/venvs/combinact
