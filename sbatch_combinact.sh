2a1a19ab390319307a1a55b6800a35c7254c985a
rearle <rb504035@dal.ca>
2020-08-13 23:39:48 -0300

    Job: e3_nparam_log
    Varying number of parameters on log scale (fixed bug)

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=8G               # memory per node
 #SBATCH --time=20:00:00        # max walltime, hh:mm:ss
 #SBATCH --array=0-39%8        # array value
-#SBATCH --output=logs/bin_peff/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=bin_peff
+#SBATCH --output=logs/e3_nparam_log/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e3_nparam_log
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -33,9 +33,9 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset mnist --actfun bin --p_param_eff --var_n_params_log
-python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar10 --actfun bin --p_param_eff --var_n_params_log
-python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar100 --actfun bin --p_param_eff --var_n_params_log
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset mnist --actfun bin --p_param_eff --var_n_params_log
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar10 --actfun bin --p_param_eff --var_n_params_log
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar100 --actfun bin --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset mnist --actfun all_pk_relu_combinact --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar10 --actfun all_pk_relu --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar100 --actfun all_pk_relu --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset mnist --actfun all_pk_relu_combinact --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar10 --actfun all_pk_relu_combinact --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar100 --actfun all_pk_relu_combinact --var_n_params_log
