ac09aa35aacb42416e9e21b022c55d506a1dd217
rearle <rb504035@dal.ca>
2020-08-13 23:36:49 -0300

    Job: bin_peff
    Binary operations network, testing parameter efficiency

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=8G               # memory per node
 #SBATCH --time=20:00:00        # max walltime, hh:mm:ss
 #SBATCH --array=0-39%8        # array value
-#SBATCH --output=logs/bin_nsamples/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=bin_nsamples
+#SBATCH --output=logs/bin_peff/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=bin_peff
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -33,9 +33,9 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset mnist --actfun bin --var_n_samples
-python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar10 --actfun bin --var_n_samples
-python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar100 --actfun bin --var_n_samples
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset mnist --actfun bin --var_n_samples
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar10 --actfun bin --var_n_samples
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar100 --actfun bin --var_n_samples
+python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset mnist --actfun bin --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar10 --actfun bin --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar100 --actfun bin --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset mnist --actfun bin --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar10 --actfun bin --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar100 --actfun bin --p_param_eff --var_n_params_log
