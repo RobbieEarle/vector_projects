f618ba9fdb81a575a4ca2d8f9ba6bda8c0ef79b7
rearle <rb504035@dal.ca>
2020-08-14 14:11:43 -0300

    Jobs: e3_rands_comb4
    Fourth iteration of random search for combinact

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -5,9 +5,9 @@
 #SBATCH -c 4                   # number of CPU cores
 #SBATCH --mem=8G               # memory per node
 #SBATCH --time=20:00:00        # max walltime, hh:mm:ss
-#SBATCH --array=0-95%8        # array value
-#SBATCH --output=logs/e3_rands_comb3/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e3_rands_comb3
+#SBATCH --array=0-95%8         # array value
+#SBATCH --output=logs/e3_rands_comb4/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e3_rands_comb4
 
 source ~/.bashrc
 source activate ~/venvs/combinact
