158efab40255fc8321a7e3224afa93ad27ccf4ed
rearle <rb504035@dal.ca>
2020-08-26 00:17:02 -0300

    Job: e4_peff_log
    Re-running peff experiment

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -5,10 +5,10 @@
 #SBATCH --qos=nopreemption
 #SBATCH -c 4                   # number of CPU cores
 #SBATCH --mem=8G               # memory per node
-#SBATCH --time=20:00:00        # max walltime, hh:mm:ss
+#SBATCH --time=30:00:00        # max walltime, hh:mm:ss
 #SBATCH --array=0-39%8        # array value
-#SBATCH --output=logs/e4_nparam_log/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e4_nparam_log
+#SBATCH --output=logs/e4_peff_log/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e4_peff_log
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -31,9 +31,9 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset mnist --actfun all_pk_comb_relu --var_n_params_log
-python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar10 --actfun combinact --var_n_params_log
-python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar100 --actfun combinact --var_n_params_log
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset mnist --actfun all_pk_comb_relu --var_n_params_log
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar10 --actfun all_pk_comb_relu --var_n_params_log
-python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar100 --actfun all_pk_comb_relu --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset mnist --actfun all_pk_relu --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar10 --actfun all_pk_relu --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model mlp --dataset cifar100 --actfun all_pk_relu --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset mnist --actfun all_pk_relu --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar10 --actfun all_pk_relu --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --model cnn --dataset cifar100 --actfun all_pk_relu --p_param_eff --var_n_params_log
