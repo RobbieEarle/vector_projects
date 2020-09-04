f5cd704f6136da5178e316a60b9fa59822eb3586
rearle <rb504035@dal.ca>
2020-09-04 20:32:35 -0300

    Job: e5_peff
    Just running CNN CIFAR100

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -5,10 +5,10 @@
 #SBATCH --qos=normal
 #SBATCH -c 4                                # number of CPU cores
 #SBATCH --mem=8G                            # memory per node
-#SBATCH --time=30:00:00                     # max walltime, hh:mm:ss
+#SBATCH --time=20:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0-39%8                      # array value
-#SBATCH --output=logs/e5_nparam/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e5_nparam
+#SBATCH --output=logs/e5_peff/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e5_peff
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -36,4 +36,4 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model $MODEL --dataset $DATASET --actfun all_pk_comb_relu --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model $MODEL --dataset $DATASET --actfun all_pk_comb_relu --p_param_eff --var_n_params_log
