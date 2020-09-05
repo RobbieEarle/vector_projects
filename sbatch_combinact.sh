1b7e4f0236ac09511cdda2db0b0b13ac539b5d47
rearle <rb504035@dal.ca>
2020-09-04 23:12:58 -0300

    Job: e5_resnet
    Testing out ResNet architecture with higher order actfuns. Simple first
    run using CNN hyper-params (ie. hyper params haven't been optimized)

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -7,14 +7,13 @@
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=20:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0-39%8                      # array value
-#SBATCH --output=logs/e5_vark/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e5_vark
+#SBATCH --output=logs/e5_resnet/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e5_resnet
 
 source ~/.bashrc
 source activate ~/venvs/combinact
 
 SAVE_PATH="$1"
-DATASET="$2"
 SEED="$SLURM_ARRAY_TASK_ID"
 
 touch /checkpoint/robearle/${SLURM_JOB_ID}
@@ -35,5 +34,6 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset $DATASET --actfun all_pk_relu --var_k
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset $DATASET --actfun all_pk_relu --var_k
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset mnist --actfun all_pk_comb_relu
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar10 --actfun all_pk_comb_relu
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar100 --actfun all_pk_comb_relu
