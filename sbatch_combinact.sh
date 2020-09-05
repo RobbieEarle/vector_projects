56f4a47470bff9423efb6bf2c186e8235f214dc0
rearle <rb504035@dal.ca>
2020-09-04 23:03:48 -0300

    Job: e5_vark
    Re-running vary k experiment with new generalization gap metric

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -7,8 +7,8 @@
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=20:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0-39%8                      # array value
-#SBATCH --output=logs/e5_bin_peff/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e5_bin_peff
+#SBATCH --output=logs/e5_vark/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e5_vark
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -35,4 +35,5 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset $DATASET --actfun bin --p_param_eff --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset $DATASET --actfun all_pk_relu --var_k
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset $DATASET --actfun all_pk_relu --var_k
