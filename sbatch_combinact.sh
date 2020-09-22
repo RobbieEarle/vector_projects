47d6e5adf853bfb713ed96e23f674eb3699a5794
rearle <rb504035@dal.ca>
2020-09-21 21:55:26 -0300

    Job: resnet_test-t6
    Trying without augmentation, stripped down ResNet, small
    Logging lr as we train
    Removed normalization
    Batch size = 128

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=30:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0%1                      # array value
-#SBATCH --output=logs/resnet_test/t5-%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --output=logs/resnet_test/t6-%a-%N-%j    # %N for node name, %j for jobID
 #SBATCH --job-name=resnet_test
 
 source ~/.bashrc
@@ -34,4 +34,4 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar10 --actfun relu --resnet_ver 34  --num_epochs 30 --resnet_orig
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar10 --actfun relu --resnet_ver 34  --num_epochs 30 --resnet_orig --batch_size 128
