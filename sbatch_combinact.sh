8274036ac16d56bcd167fa61dc91925d69a5fd8c
rearle <rb504035@dal.ca>
2020-10-05 13:11:03 -0300

    Job: e9_nparam
    Re-running nparam experiment for SwishK', Swish, and Swishk' inverted

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -6,9 +6,9 @@
 #SBATCH -c 4                                # number of CPU cores
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=30:00:00                     # max walltime, hh:mm:ss
-#SBATCH --array=0-95%12                     # array value
-#SBATCH --output=logs/e8_swish_rs4/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e8_swish_rs4
+#SBATCH --array=0-39%8                      # array value
+#SBATCH --output=logs/e9_nparam/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e9_nparam
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -34,9 +34,10 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk_p --num_epochs 10
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk_p --num_epochs 10
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swish --num_epochs 10 --k 1
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swish --num_epochs 10 --k 1
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk_p --p 2 --perm_method invert --num_epochs 10 --label _inv
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk_p --p 2 --perm_method invert --num_epochs 10 --label _inv
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk_p --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk --k 2 --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk_p --p 2 --perm_method invert --var_n_params_log --label _inv
+
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk_p --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk --k 2 --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk_p --p 2 --perm_method invert --var_n_params_log --label _inv
