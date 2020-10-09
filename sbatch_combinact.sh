7837035e353113fa5ba817b00cc795f7a2f25093
rearle <rb504035@dal.ca>
2020-10-09 13:36:47 -0300

    Job: e8_resnet_rs6
    Running final iteration again because some results did not converge.

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -6,9 +6,9 @@
 #SBATCH -c 4                                # number of CPU cores
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=30:00:00                     # max walltime, hh:mm:ss
-#SBATCH --array=0%1                         # array value
-#SBATCH --output=logs/e8_resnet_rs5/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e8_resnet_rs5
+#SBATCH --array=45-49%5                       # array value
+#SBATCH --output=logs/e8_resnet_rs6/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e8_resnet_rs6
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -36,7 +36,7 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed 0 --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset mnist --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
-python engine.py --seed 0 --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar10 --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
-python engine.py --seed 0 --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar100 --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset mnist --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar10 --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar100 --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
 
