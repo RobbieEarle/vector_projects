629cda81a89848a98c9509cd235eac7958cbce2b
rearle <rb504035@dal.ca>
2020-10-10 14:30:41 -0300

    Job: e8_resnet_rs7
    Fixed another bug, was taking hyper-params as e^x instead of 10^x

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -6,16 +6,16 @@
 #SBATCH -c 4                                # number of CPU cores
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=30:00:00                     # max walltime, hh:mm:ss
-#SBATCH --array=0-27%4                      # array value
-#SBATCH --output=logs/e8_swishy_inv/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e8_swishy_inv
+#SBATCH --array=0-4%5                       # array value
+#SBATCH --output=logs/e8_resnet_rs7/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e8_resnet_rs7
 
 source ~/.bashrc
 source activate ~/venvs/combinact
 
 SAVE_PATH="$1"
-MODEL="$2"
-DATASET="$3"
+ACTFUN="$2"
+HP_IDX="$3"
 SEED="$SLURM_ARRAY_TASK_ID"
 
 touch /checkpoint/robearle/${SLURM_JOB_ID}
@@ -36,4 +36,7 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model $MODEL --dataset $DATASET --actfun swishk_p --num_epochs 10 --p 2 --perm_method invert
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset mnist --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar10 --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar100 --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
+
