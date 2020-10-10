d8f1e7211522d31db841532ca3832abf4374ed09
rearle <rb504035@dal.ca>
2020-10-10 20:49:03 -0300

    Job: e9_swish_vark2
    Running varying k experiment with swishY_inv

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -6,9 +6,9 @@
 #SBATCH -c 4                                # number of CPU cores
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=30:00:00                     # max walltime, hh:mm:ss
-#SBATCH --array=0-39%8                      # array value
-#SBATCH --output=logs/e9_swish_vark/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e9_swish_vark
+#SBATCH --array=0-39%4                      # array value
+#SBATCH --output=logs/e9_swish_vark2/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e9_swish_vark2
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -34,8 +34,5 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk_p --var_k
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swish --k 1 --var_k
-
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk_p --var_k
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swish --k 1 --var_k
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk_p --p 2 --perm_method invert --var_k 
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk_p --p 2 --perm_method invert --var_k
