284bcec681cdadf12d3426c4f346de5b17fe6cb4
rearle <rb504035@dal.ca>
2020-10-10 20:06:12 -0300

    Job: e9_swish_nparam
    Running varying number parameters experiment with swish and swishY

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -6,16 +6,14 @@
 #SBATCH -c 4                                # number of CPU cores
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=30:00:00                     # max walltime, hh:mm:ss
-#SBATCH --array=0-4%5                       # array value
-#SBATCH --output=logs/e8_resnet_rs7/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e8_resnet_rs7
+#SBATCH --array=0-39%8                      # array value
+#SBATCH --output=logs/e9_swish_nparam/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e9_swish_nparam
 
 source ~/.bashrc
 source activate ~/venvs/combinact
 
 SAVE_PATH="$1"
-ACTFUN="$2"
-HP_IDX="$3"
 SEED="$SLURM_ARRAY_TASK_ID"
 
 touch /checkpoint/robearle/${SLURM_JOB_ID}
@@ -36,7 +34,8 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset mnist --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar10 --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model resnet --dataset cifar100 --actfun $ACTFUN --resnet_ver 34 --resnet_width 2 --num_epochs 50 --hp_idx $HP_IDX --label $HP_IDX
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk_p --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swish --k 1 --var_n_params_log
 
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk_p --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swish --k 1 --var_n_params_log
