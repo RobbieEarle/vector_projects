594eedf28a6c04efd539aa2c395769acdb81d1a2
rearle <rb504035@dal.ca>
2020-10-10 20:58:30 -0300

    Job: e9_swish_nparam2
    Running varying number of params experiment with swishY_inv

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -7,8 +7,8 @@
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=30:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0-39%4                      # array value
-#SBATCH --output=logs/e9_swish_vark2/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e9_swish_vark2
+#SBATCH --output=logs/e9_swish_nparam2/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e9_swish_nparam2
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -34,5 +34,5 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk_p --p 2 --perm_method invert --var_k 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk_p --p 2 --perm_method invert --var_k
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk_p --p 2 --perm_method invert --var_n_params_log
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk_p --p 2 --perm_method invert --var_n_params_log
