133d70f0ddeafc00848e093d75f8377bdc6151e4
rearle <rb504035@dal.ca>
2020-10-12 16:30:34 -0300

    Job: e9_swish_nparam3
    Vary number of parameters for SwishK_inv only

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -8,14 +8,13 @@
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=30:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0-39%8                      # array value
-#SBATCH --output=logs/e9_swish_vark3/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=e9_swish_vark3
+#SBATCH --output=logs/e9_swish_nparam3/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=e9_swish_nparam3
 
 source ~/.bashrc
 source activate ~/venvs/combinact
 
 SAVE_PATH="$1"
-MODEL="$2"
 SEED="$SLURM_ARRAY_TASK_ID"
 
 touch /checkpoint/robearle/${SLURM_JOB_ID}
@@ -36,6 +35,5 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model $MODEL --dataset mnist --actfun swishk_p --var_k
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model $MODEL --dataset mnist --actfun swishk_p --perm_method invert --var_k --label _inv
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model $MODEL --dataset mnist --actfun swishk --perm_method invert --var_k --label _inv
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model mlp --dataset mnist --actfun swishk --perm_method invert --var_n_params_log --label _inv
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --model cnn --dataset mnist --actfun swishk --perm_method invert --var_n_params_log --label _inv
