0e05ac41cc004e99dc5034d8da0704108c6a6e12
rearle <rb504035@dal.ca>
2020-10-19 22:57:27 -0300

    Job: rms2_resnet_lr001
    Re-trying rmsprop with ResNet, bug fixed with data augmentation. Made
    initial LR fixed for this experiment

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -9,7 +9,7 @@
 #SBATCH --time=30:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0%1                         # array value
 #SBATCH --output=logs/rms2_resnet_lr001/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=rms2_resnet_lr
+#SBATCH --job-name=rms2_resnet_lr001
 
 source ~/.bashrc
 source activate ~/venvs/combinact
