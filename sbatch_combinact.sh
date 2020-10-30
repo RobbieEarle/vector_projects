019fd2f7f1a8568831659000ca089942af568dda
rearle <rb504035@dal.ca>
2020-10-30 15:16:43 -0300

    Job: lr_range7
    LR range test zoomed with val_loader for evaluation. Now using min
    loss value to determine y bounds. Fixed bug

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -8,14 +8,15 @@
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=40:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0%1                         # array value
-#SBATCH --output=logs/lr_range6/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=lr_range6
+#SBATCH --output=logs/lr_range7/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=lr_range7
 
 source ~/.bashrc
 source activate ~/venvs/combinact
 
 SAVE_PATH="$1"
 ACTFUN="$2"
+SEED="$3"
 GRID_ID="$SLURM_ARRAY_TASK_ID"
 
 touch /checkpoint/robearle/${SLURM_JOB_ID}
@@ -32,12 +33,11 @@ python -c "import torch; print('torch version = {}'.format(torch.__version__))"
 python -c "import torch.cuda; print('cuda = {}'.format(torch.cuda.is_available()))"
 python -c "import scipy; print('scipy version = {}'.format(scipy.__version__))"
 python -c "import sklearn; print('sklearn version = {}'.format(sklearn.__version__))"
-python -c "import torch_lr_finder; print('torch_lr_finder version = {}'.format(torch_lr_finder.__version__))"
 python -c "import matplotlib; print('matplotlib version = {}'.format(matplotlib.__version__))"
 echo ""
 
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed 0 --save_path $SAVE_PATH --check_path $CHECK_DIR --optim onecycle --model resnet --dataset cifar100 --actfun $ACTFUN --validation --lr_range
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_DIR --optim onecycle --model resnet --dataset cifar100 --actfun $ACTFUN --validation --lr_range
 
