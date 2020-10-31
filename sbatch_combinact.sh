8b83503b59d874c0a69896476c821eebf3c47119
rearle <rb504035@dal.ca>
2020-10-31 15:17:51 -0300

    Job: test_max
    Running max activation on test set with optimized hyper-params

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -7,7 +7,7 @@
 #SBATCH -c 4                                # number of CPU cores
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=40:00:00                     # max walltime, hh:mm:ss
-#SBATCH --array=20-39%20                        # array value
+#SBATCH --array=20-39%5                        # array value
 #SBATCH --output=logs/test_max/%a-%N-%j    # %N for node name, %j for jobID
 #SBATCH --job-name=test_max
 
