c7fcd1ed1b204bbc135aff82d1c56ca707d3934b
rearle <rb504035@dal.ca>
2020-11-01 17:40:18 -0400

    Job: rs2_ail
    Second random search iteration for ail actfuns

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -8,8 +8,8 @@
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=40:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0-99%25                        # array value
-#SBATCH --output=logs/rs1_ail/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=rs1_ail
+#SBATCH --output=logs/rs2_ail/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=rs2_ail
 
 source ~/.bashrc
 source activate ~/venvs/combinact
