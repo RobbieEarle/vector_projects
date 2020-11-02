67f7f8ded5016731fa6e460d2d84229b10809304
rearle <rb504035@dal.ca>
2020-11-02 02:41:27 -0400

    Job: e11_swish_nparam3
    Re-running entire swish experiment again. Last time we were running
    augmentations on the mnist dataset, which was not done the first
    time we ran this experiment

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -7,7 +7,7 @@
 #SBATCH -c 4                                # number of CPU cores
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=40:00:00                     # max walltime, hh:mm:ss
-#SBATCH --array=0-39%20                     # array value
+#SBATCH --array=0-39%10                     # array value
 #SBATCH --output=logs/e11_swish_nparam3/%a-%N-%j    # %N for node name, %j for jobID
 #SBATCH --job-name=e11_swish_nparam3
 
