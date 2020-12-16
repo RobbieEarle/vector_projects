8dfd611bd22652efd5434ffa160f82292ef01566
rearle <rb504035@dal.ca>
2020-12-16 14:22:46 -0400

    Job: hparam_rs1b
    50 epoch sbatch file
    
    First hyper-parameter search for comparison to one-shot.
    Only using Max and ReLU on CIFAR100 for 10, 50, and 100 epochs.
    Fixed low weight decay ranges.

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -7,7 +7,7 @@
 #SBATCH -c 4                                # number of CPU cores
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=40:00:00                     # max walltime, hh:mm:ss
-#SBATCH --array=0-119%5                    # array value
+#SBATCH --array=0-119%20                    # array value
 #SBATCH --output=logs_new/hparam_rs1b/10e/%a-%N-%j    # %N for node name, %j for jobID
 #SBATCH --job-name=hparam_rs1b
 
