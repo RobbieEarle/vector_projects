4a57f9786ae6b71b9e2709c1297294253b4b4337
rearle <rb504035@dal.ca>
2020-12-17 17:42:45 -0400

    Job: hparam_rs4
    10 epoch sbatch file
    
    Fourth hyper-parameter search for comparison to one-shot.
    Only using Max and ReLU on CIFAR100 for 10, 50, and 100 epochs.
    Fixed low weight decay ranges.

--- sbatch_combinact.sh
+++ sbatch_combinact.sh
@@ -7,9 +7,9 @@
 #SBATCH -c 4                                # number of CPU cores
 #SBATCH --mem=8G                            # memory per node
 #SBATCH --time=40:00:00                     # max walltime, hh:mm:ss
-#SBATCH --array=0-119%40                     # array value
-#SBATCH --output=logs_new/hparam_rs3/100e/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=hparam_rs3
+#SBATCH --array=0-119%10                     # array value
+#SBATCH --output=logs_new/hparam_rs4/10e/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=hparam_rs4
 
 source ~/.bashrc
 source activate ~/venvs/combinact
