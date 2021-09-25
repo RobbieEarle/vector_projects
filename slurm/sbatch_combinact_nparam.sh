1c31325edf4aab95635ee520acfe30e1cf18e62a
robert.earle <robert.earle@verafin.com>
2021-09-25 16:44:35 -0300

    Job: wrn50_cf100
    Running wide resnet 50 on CIFAR100 for the table. 20 seeds.

--- slurm/sbatch_combinact_nparam.sh
+++ slurm/sbatch_combinact_nparam.sh
@@ -2,12 +2,12 @@
 #SBATCH -p p100,t4v1,t4v2
 #SBATCH --gres=gpu:1                        # request GPU(s)
 #SBATCH --qos=normal
-#SBATCH -c 24                                # number of CPU cores
-#SBATCH --mem=128G                            # memory per node
+#SBATCH -c 6                                # number of CPU cores
+#SBATCH --mem=32G                            # memory per node
 #SBATCH --time=700:00:00                     # max walltime, hh:mm:ss
 #SBATCH --array=0-10%11                    # array value
-#SBATCH --output=logs_new/wrn_50_nparam4/%a-%N-%j    # %N for node name, %j for jobID
-#SBATCH --job-name=wrn_50_nparam4
+#SBATCH --output=logs_new/wrn50_cf100/%a-%N-%j    # %N for node name, %j for jobID
+#SBATCH --job-name=wrn50_cf100
 
 source ~/.bashrc
 source activate ~/venvs/combinact
@@ -15,7 +15,7 @@ source activate ~/venvs/combinact
 SEED="$1"
 ACTFUN_IDX="$SLURM_ARRAY_TASK_ID"
 
-SAVE_PATH=~/vector_projects/outputs/wrn_50_nparam4
+SAVE_PATH=~/vector_projects/outputs/wrn50_cf100
 CHECK_PATH="/checkpoint/$USER/${SLURM_JOB_ID}"
 touch $CHECK_PATH
 
@@ -36,7 +36,4 @@ echo ""
 echo "SAVE_PATH=$SAVE_PATH"
 echo "SEED=$SEED"
 
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_PATH --model resnet --batch_size 128 --actfun_idx $ACTFUN_IDX --optim onecycle --num_epochs 100 --dataset cifar100 --aug --mix_pre_apex --bs_factor 0.75 --c 80
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_PATH --model resnet --batch_size 128 --actfun_idx $ACTFUN_IDX --optim onecycle --num_epochs 100 --dataset cifar100 --aug --mix_pre_apex --bs_factor 0.75 --c 25
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_PATH --model resnet --batch_size 128 --actfun_idx $ACTFUN_IDX --optim onecycle --num_epochs 100 --dataset cifar100 --aug --mix_pre_apex --bs_factor 0.75 --c 8
-python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_PATH --model resnet --batch_size 128 --actfun_idx $ACTFUN_IDX --optim onecycle --num_epochs 100 --dataset cifar100 --aug --mix_pre_apex --bs_factor 0.75 --c 3
+python engine.py --seed $SEED --save_path $SAVE_PATH --check_path $CHECK_PATH --model resnet --batch_size 128 --actfun_idx $ACTFUN_IDX --optim onecycle --num_epochs 100 --dataset cifar100 --aug --mix_pre_apex --bs_factor 0.75
