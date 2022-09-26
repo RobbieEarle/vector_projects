2381ec42ec908c4b19a98c1a66c6935cdc3a4503
RobbieEarle <rb504035@dal.ca>
2022-09-26 12:30:33 -0300

    Reducing batch size to 1/4

--- trainer.py
+++ trainer.py
@@ -251,8 +251,8 @@ def train(args, checkpoint, mid_checkpoint_location, final_checkpoint_location, 
                 n += 1
                 with amp.scale_loss(train_loss, optimizer) as scaled_loss:
                     scaled_loss.backward()
-                if args.split_batch and (batch_idx+1)%2==0:
-                    train_loss = train_loss/2
+                if args.split_batch and (batch_idx+1)%4==0:
+                    train_loss = train_loss/4
                     optimizer.step()
                     optimizer.zero_grad()
                 elif not args.split_batch:
